0c830ea193850428ee9ea70172eeed2f25512e8f
SatoruLess <46799370+SatoruLess@users.noreply.github.com>
2020-03-11 17:43:32 -0300

    Create Complexo.c

---
+++ Complexo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Tcomplex {
+    int Real;
+    int Imag;
+
+} Tcomplex;
+
+Tcomplex Complexo(int A, int B){
+    Tcomplex C1;
+    C1
+}
+
+int main (){
+    
+    int i1, i2, i3, i4;
+    char a1, b2, c3;
+    
+    while (scanf ("%d", &i1) != EOF) {
+        scanf ("%d %c %c %d %d %c", &i2, &a1, &a2, &i3, &i4, &a3);
+        
+
+
+    }
+    return 0;
+}
