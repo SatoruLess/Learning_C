121aac66019777cdfd6806a73b39ca227604d027
SatoruLess <46799370+SatoruLess@users.noreply.github.com>
2020-03-11 18:57:20 -0300

    dsdsd

--- Complexo.c
+++ Complexo.c
@@ -1,6 +1,6 @@
-  
+
 #include <stdio.h>
-#include <stdlib.h>
+
 
 typedef struct Tcomplex {
     int Real;
@@ -26,7 +26,7 @@ Tcomplex sub(Tcomplex C1,Tcomplex C2){
     C3.Imag = C1.Imag - C2.Imag;
     return C3;
 }
-Tcomplex multi(Tcomplex C1,Tcomplex C2){
+Tcomplex mult(Tcomplex C1,Tcomplex C2){
     Tcomplex C3;
     C3.Real = (C1.Real*C2.Real) - (C1.Imag*C2.Imag);
     C3.Imag = (C1.Real*C2.Imag) + (C1.Imag*C2.Real);
@@ -40,15 +40,15 @@ Tcomplex div(Tcomplex C1,Tcomplex C2){
 }
 
 int main (){
-    
+
     int r1, r2, i1, i2;
     char a1, a2, a3;
     Tcomplex C1,C2,C3;
 
     while (scanf ("%d", &r1) != EOF) {
         scanf ("%d %c %c %d %d %c", &i1, &a1, &a2, &r2, &i2, &a3);
-        C1 = Complexo(r1,i1);
-        C2 = Complexo(r2,i2);
+        C1 = complexo(r1,i1);
+        C2 = complexo(r2,i2);
         if (a2 == '+'){
             C3 = soma(C1,C2);
         }
